b51858893360f6b5107b3f3741e03eca16357464
hiram_chavez <hiram.chavez@dataiq.com.mx>
2024-10-01 09:07:54 -0600

    Implement with Thymeleaf

---
+++ src/main/java/tech/hiramchavez/oauth/controllers/MainController.java
@@ -0,0 +1,20 @@
+package tech.hiramchavez.oauth.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+}
---
+++ src/test/java/tech/hiramchavez/oauth/LearningSpringOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hiramchavez.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
